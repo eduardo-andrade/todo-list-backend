46523e645f2355efb5b8867294d05204bf22abdf
Luiz Andrade <eduardoandrade@me.com>
2021-12-08 09:00:20 -0300

    Update folders schema and files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+require("./app/database");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`App Started on Port ${PORT}!`));
+
+//
